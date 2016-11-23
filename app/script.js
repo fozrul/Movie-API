cbf118c948f11a62f09837a7b2f56b1cb2a25a0a
fozrul <fozrul@hotmail.co.uk>
2016-11-23 09:22:15 +0000

    Added comments to script.js

--- app/script.js
+++ app/script.js
@@ -1,4 +1,5 @@
 //this determines if the request is good or bad etc
+// this returns a 200 Ok which is a very good response as it shows the request has succeeded
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=fef1753927fd3fd57cb0d1ef20a63fb8", false);
