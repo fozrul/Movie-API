41c1b8c6e661c76c8e24aee141d28d9bed898ba2
fozrul <fozrul@hotmail.co.uk>
2016-12-14 03:53:56 +0000

    added test to my api to test get films

--- app/indexStore.js
+++ app/indexStore.js
@@ -2,7 +2,7 @@
 
 const file = require('./storage')
     // passing a valid movie name
-file.addMovie('dead pool', (err, movie) => {
+file.addMovie('arrival', (err, movie) => {
     if (err) {
         console.log('ERROR')
         console.log(err.message)
